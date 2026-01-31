4d86f32da1b01d762065ef54446d5016cb38cc75
Just Orange <nfilimonov135@gmail.com>
2026-01-31 14:56:21 +0300

    Small changes
    Edit build file

--- Engine/Build.bat
+++ Engine/Build.bat
@@ -1,3 +1,2 @@
-cd D:\RECREATE_UNREAL_ENGINE\Workspace\FuzzumEngine_RC
-Engine\Binaries\x64\Release\FuzzumBuildTool.exe "Engine\Source\FuzzumEngine.Target.cs" "Engine"
+Binaries\x64\Release\FuzzumBuildTool.exe "Engine\Source\FuzzumEngine.Target.cs" "Engine"
 pause
